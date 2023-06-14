3c8343ecfaf1d8c4057dd58fbbe7cd574276a170
vuduc-bic <vuduc@evol.vn>
2023-06-14 10:18:32 +0700

    add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,33 @@
 module.exports = {
-    apps: [
-      {
-        name: 'dashboard',
-        script : "./App.jsx",
-        args: 'start -p 5555',
-  
-        instances: 'max',
-        exec_mode: 'cluster',
-  
-        watch: false,
-        //watch_delay: 1000,
-        //ignore_watch: ["node_modules", "src/public"],
-  
-        autorestart: true,
-        max_restarts: 10,
-        restart_delay: 5000,
-      },
-    ],
-  };
-  
+  apps: [
+    {
+      name: 'dash-board',
+      script: 'npm',
+      args: 'start -p 5555',
+      cwd: '/path/to/your/project',
+      watch: true,
+      ignore_watch: ['node_modules', 'dist']
+    },
+    {
+      script: 'index.js',
+      watch: '.'
+    },
+    {
+      script: './service-worker/',
+      watch: ['./service-worker']
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
