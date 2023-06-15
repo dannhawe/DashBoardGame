cf897da9af0160701c92a543921ffbd42bf72a7e
vuduc-bic <vuduc@evol.vn>
2023-06-15 13:26:28 +0700

    1

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "",
   plugins: [react()],
   resolve: {
+    // Resolve the 'module' entrypoint at all times (not the default due to Node.js compatibility issues).
+    mainFields: ["module"],
     alias: [{ find: "@", replacement: "/src" }],
+  },
+    build: {
+    target: "ES2022",
   },
 });
