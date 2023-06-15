a2cbe2e0878230ac02b2ce7930343e7421e566d2
vuduc-bic <vuduc@evol.vn>
2023-06-15 13:02:21 +0700

    a

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ export default defineConfig({
   base: "",
   plugins: [react()],
   resolve: {
+    // Resolve the 'module' entrypoint at all times (not the default due to Node.js compatibility issues).
+    mainFields: ["module"],
     alias: [{ find: "@", replacement: "/src" }],
+  },
+  build: {
+    target: "ES2022",
   },
 });
